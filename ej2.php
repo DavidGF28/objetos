6e9525066214017a50f558f8e2c6dcc19d4a95f6
DavidGF <DavidGuFu@gmail.com>
2019-12-17 13:21:50 +0100

    objetos ej2 modificado

--- ej2.php
+++ ej2.php
@@ -6,6 +6,8 @@ spl_autoload_register(function ($nombre_clase) {
 
 use clases\Vector;
 
-$a=new Vector();
-
+$a=new Vector([1,1,1,2,2,2,3]);
+$a->imprimirValores();
+echo "el valor maximo es: ". $a->getMaximo();
+echo "La Moda es: ".$a->getModa();
 ?>
